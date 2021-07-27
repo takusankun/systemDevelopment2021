26c39118f92a046f456f38c9510150b2228d60f5
takusankun <cath1200223@gn.iwasaki.ac.jp>
2021-07-27 23:05:19 +0900

    練習 02_2

---
+++ src/jp/ac/isc/cloud/UserDeleteServlet.java
@@ -0,0 +1,39 @@
+package jp.ac.isc.cloud;
+
+import java.sql.*;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/UserDeleteServlet")
+public class UserDeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			Connection users = null;
+			try {
+				request.setCharacterEncoding("utf-8");
+				Class.forName("com.mysql.jdbc.Driver");
+				users = DriverManager.getConnection("jdbc:mysql://localhost/servlet_db","root","");
+				String id = request.getParameter("deleteId");
+				Statement state = users.createStatement();
+				state.executeUpdate("DELETE FROM user_table WHERE id='" + id + "'");
+				state.close();
+				users.close();
+				response.sendRedirect("/select"); //UserSelectServletを呼び出す
+			}catch(ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+	}
+}
